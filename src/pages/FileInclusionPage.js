3195a2cc1dceef3ad03a64aa4681e845921c2f26
hanane54 <hanane59taouil@gmail.com>
2022-06-02 01:24:36 +0100

    Added: the fileInclusion configuration

--- src/pages/FileInclusionPage.js
+++ src/pages/FileInclusionPage.js
@@ -4,21 +4,40 @@ import styles from './FileInclusionPage.module.css'
 
 const FileInclusionPage = () => {
   const { fileName } = useParams();
+//  without protection against file inclusion  
+//   useEffect(() => {
+//     const div = document.getElementById("inclusionFileSection");
+//     fetch(`/${fileName}`)
+//       .then((response) => response.text())
+//       .then((data) => {
+//         div.innerHTML = data;
+//       });
+//   }, []);
+
+
+// with protection against the file inclusion
+// to prevent file inclusion we should check the file name
+// to only allow the name of the wanted file. other than that we 
   useEffect(() => {
     const div = document.getElementById("inclusionFileSection");
-    fetch(`/${fileName}`)
+    if( fileName === "fileInclusion.html" ){
+        fetch(`/${fileName}`)
       .then((response) => response.text())
       .then((data) => {
         div.innerHTML = data;
       });
+    } else {
+        div.innerHTML = "";
+    }
   }, []);
+
   return (
     <div>
-    <div id="inclusionFileSection" className={styles.includedFile}></div>
 
       <div className={styles.card}>
           <h3>Vulnerability: File Inclusion</h3>
           <div className={styles.desc}>To include a file edit the ?page=index.php in the URL to determine which file is included.</div>
+          <div id="inclusionFileSection" className={styles.includedFile}></div>
       </div>
     </div>
   );
